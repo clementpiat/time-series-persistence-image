44ecbeb83b96473993f3d6a591ba428ec07a0785
clementpiat <clement.piat2@gmail.com>
2021-05-19 17:17:15 +0200

    add License

--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
 from setuptools import find_packages, setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='ts2pi',
     packages=find_packages(include=['ts2pi']),
     version='0.1.0',
     description='Python module to transform a time series into a persistence image representation',
     author='Clement Piat',
-    license='MIT',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     install_requires=[
         'numpy==1.20.3',
         'gudhi==3.4.1',
